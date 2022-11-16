d8946dd0a96f09fdaf0738ffce6d0e82a75d1669
Samuel Oluwatosin ONIYILO <masterarcher6321@gmail.com>
2022-11-16 15:34:12 +0100

    MyStore completed
    List of contacts in my store

---
+++ MyStore/MyStore/Pages/Clients/Edit.cshtml.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class EditModel : PageModel
+    {
+        public ClientInfo clientInfo = new ClientInfo();
+        public string errorMessage = "";
+        public string successMessage = "";
+
+        public void OnGet()
+        {
+            string id = Request.Query["id"];
+
+            try
+            {
+                String connectionString = "Data Source=.;Initial Catalog=mystore;Integrated Security=True";
+                using (SqlConnection cnn = new SqlConnection(connectionString))
+                {
+                    cnn.Open();
+                    String sql = "SELECT * FROM clients WHERE id=@id";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone =  reader.GetString(3);
+                                clientInfo.address =  reader.GetString(4);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message;
+            }
+        }
+
+        public void OnPost()
+        {
+            clientInfo.id = Request.Form["id"];
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if (clientInfo.id.Length == 0 || clientInfo.name.Length == 0 || clientInfo.email.Length == 0 ||
+               clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            {
+                errorMessage = "All fields are required";
+                return;
+            }
+
+            try
+            {
+                String connectionString = "Data Source=.;Initial Catalog=mystore;Integrated Security=True";
+                using (SqlConnection cnn = new SqlConnection(connectionString))
+                {
+                    cnn.Open();
+                    String sql = " UPDATE clients " +
+                                 " SET name=@name, email=@email, phone=@phone, address=@address " +
+                                 " WHERE id=@id ";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, cnn))
+                    {
+                        cmd.Parameters.AddWithValue("@name", clientInfo.name);
+                        cmd.Parameters.AddWithValue("@email", clientInfo.email);
+                        cmd.Parameters.AddWithValue("@phone", clientInfo.phone);
+                        cmd.Parameters.AddWithValue("@address", clientInfo.address);
+                        cmd.Parameters.AddWithValue("@id", clientInfo.id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                errorMessage = ex.Message;
+                return;
+            }
+
+            Response.Redirect("/Clients/Index");
+        }
+    }
+}
